e35674f8ce4353cc2ea3ab9967315ba6f4057d41
unknown <YY523@ccs.local>
2025-03-15 00:16:04 -0700

    question2.js commit

---
+++ question2.js
@@ -0,0 +1,7 @@
+function sumArray(numbers) {
+    return numbers.reduce((sum, num) => sum + num, 0);
+    //reduce is like gekuha nya ang array nya iya ireduce to a single value nya iadd niya
+}
+
+// Example usage:
+console.log(sumArray([1, 2, 3, 4, 5])); 
